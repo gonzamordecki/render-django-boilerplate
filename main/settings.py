b84642075c79ff05a38f426beb3a5974681632a4
gonzamordecki <gonzalo.mordecki@gmail.com>
2024-12-18 01:33:27 -0300

    static and settings improvments

--- main/settings.py
+++ main/settings.py
@@ -102,9 +102,6 @@ else:
         )
     }
 
-print(DATABASES, "DATABASES")
-print(os.getenv('PRODUCTION_POSTGRES_URL'), "DATABASE_URL")
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
